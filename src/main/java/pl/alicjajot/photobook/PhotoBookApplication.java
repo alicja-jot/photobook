8d159be0f5873b577373893fd9a275471f3380e4
Alicja-Jot <miedzinska93@gmail.com>
2020-05-04 20:21:05 +0200

    Initial commit

---
+++ src/main/java/pl/alicjajot/photobook/PhotoBookApplication.java
@@ -0,0 +1,13 @@
+package pl.alicjajot.photobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoBookApplication.class, args);
+	}
+
+}
